ddbd46219a404cd01e6aff2ee249589b62ea9467
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-12-05 08:33:58 +0000

    6 completed

--- js_scope_homework.js
+++ js_scope_homework.js
@@ -2,15 +2,16 @@
 //
 //Variable 'name' created/assigned to the name Keith.
 //Function 'printName' created. This function will print the string 'My name is ' plus the value of the variable 'name'.
+//
 //Result: My name is Keith
-//
-// var name = 'Keith';
-//
-// var printName = function() {
-//   console.log('My name is ' + name );
-// };
-//
-// printName();
+
+var name = 'Keith';
+
+var printName = function() {
+  console.log('My name is ' + name );
+};
+
+printName();
 
 
 
@@ -19,16 +20,17 @@
 //
 //Variable 'score' created globally + assigned value of 5.
 //Function 'result' created and 'score' variable created + reassigned value of 3. This function returns the value of the score variable created within this function's scope.
+//
 //Result: 3
-//
-// score = 5;
-//
-// var result = function() {
-//   var score = 3;
-//   return score;
-// };
-//
-// console.log(result());
+
+score = 5;
+
+var result = function() {
+  var score = 3;
+  return score;
+};
+
+console.log(result());
 
 
 
@@ -38,22 +40,23 @@
 //Array 'myAnimals' created.
 //Function 'listAnimals' created and within its scope another the myAnimals array is reassigned.
 //The function is looping through the array and returns both key/index + value.
+//
 //Result:
 //0: Ducks
 // 1: Dogs
 // 2: Lions
-//
-//
-// var myAnimals = ['Chickens', 'Cats', 'Rabbits'];
-//
-// var listAnimals = function() {
-//   myAnimals = ['Ducks', 'Dogs', 'Lions'];
-//   for(var i=0;i<myAnimals.length; i++){
-//     console.log(i + ": " + myAnimals[i]);
-//   }
-// }
-//
-// listAnimals();
+
+
+var myAnimals = ['Chickens', 'Cats', 'Rabbits'];
+
+var listAnimals = function() {
+  myAnimals = ['Ducks', 'Dogs', 'Lions'];
+  for(var i=0;i<myAnimals.length; i++){
+    console.log(i + ": " + myAnimals[i]);
+  }
+}
+
+listAnimals();
 
 
 
@@ -62,24 +65,25 @@
 //
 //Four suspect variables are created and assigned.
 // Variable 'suspectThree' is reassigned (from Keith) to 'Harvey' within the 'allSuspects' function.
+//
 //Result: Suspects include: Jay, Val, Harvey, Rick
 // Suspect three is:Keith
-//
-//
-//
-// var suspectOne = 'Jay';
-// var suspectTwo = 'Val';
-// var suspectThree = 'Keith';
-// var suspectFour = 'Rick';
-//
-// var allSuspects = function() {
-//   var suspectThree = 'Harvey'
-//   console.log('Suspects include: ' + suspectOne + ', ' + suspectTwo + ', ' + suspectThree + ', ' + suspectFour)
-// };
-//
-// allSuspects();
-// console.log( 'Suspect three is:' + suspectThree );
-//
+
+
+
+var suspectOne = 'Jay';
+var suspectTwo = 'Val';
+var suspectThree = 'Keith';
+var suspectFour = 'Rick';
+
+var allSuspects = function() {
+  var suspectThree = 'Harvey'
+  console.log('Suspects include: ' + suspectOne + ', ' + suspectTwo + ', ' + suspectThree + ', ' + suspectFour)
+};
+
+allSuspects();
+console.log( 'Suspect three is:' + suspectThree );
+
 
 
 
@@ -89,43 +93,50 @@
 //Funtion 'printName' is declared/created but not called.
 //Function detectiveInfo in created and withing its scope the detective name is changed.
 //funtion 'printName' is finally called.
+//
 //Result: Poirot.
-// 
-// var detective = {
-//   name : 'Ace Ventura',
-//   pet : 'monkey'
-// };
-//
-// var printName = function(detective) {
-//   return detective.name
-// };
-//
-// var detectiveInfo = function() {
-//   detective['name'] = 'Poirot'
-//   return printName(detective);
-// };
-//
-// console.log(detectiveInfo());
+
+var detective = {
+  name : 'Ace Ventura',
+  pet : 'monkey'
+};
+
+var printName = function(detective) {
+  return detective.name
+};
+
+var detectiveInfo = function() {
+  detective['name'] = 'Poirot'
+  return printName(detective);
+};
+
+console.log(detectiveInfo());
 
 
 
 
 // Episode 6
+//
+//Variable murdered assigned to 'rick'.
+//Funtion 'outerFunction' created and variable has now been reassigned to 'marc'.
+//Within this function, another is created: 'innerFunction' where the variable 'murderer' is once again reassigned: 'valerie'.
+//Both the variable and the innerFunction within the outerFunction are only local to within this function.
+//
+//Result: The murderer is Rick.
+var murderer = 'rick';
 
-// var murderer = 'rick';
-//
-// var outerFunction = function() {
-//   var murderer = 'marc';
-//
-//   var innerFunction = function() {
-//     murderer = 'valerie';
-//   }
-//
-//   innerFunction();
-// }
-//
-// outerFunction();
-// console.log('the murderer is ', murderer);
+var outerFunction = function() {
+  var murderer = 'marc';
+
+  var innerFunction = function() {
+    murderer = 'valerie';
+  }
+
+  innerFunction();
+}
+
+outerFunction();
+console.log('the murderer is ', murderer);
 
 
 
